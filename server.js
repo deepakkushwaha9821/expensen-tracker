f8e5c8e8463add8e9a4a3d01ee3ae5f0008c6dec
deedrop <155622024+deepakkushwaha9821@users.noreply.github.com>
2025-08-24 20:19:22 +0530

    Update server.js

--- server.js
+++ server.js
@@ -142,4 +142,7 @@ app.get("/api/auth/otps", async (req, res) => {
 
 // ---------- Start Server ----------
 const PORT = process.env.PORT || 5000;
-app.listen(PORT, () => console.log(`🚀 Server running on http://localhost:${PORT}`));
+app.listen(PORT, "0.0.0.0", () => {
+  console.log(`Server running on port ${PORT}`);
+});
+
